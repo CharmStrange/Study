cff819ed7c8a813e28560aff8a00aa1b7cdcf33d
CharmStrange <105769152+CharmStrange@users.noreply.github.com>
2023-03-22 17:15:31 +0900

    클래스 생성자 공부하며 짜본 코드

---
+++ Class2Class.cpp
@@ -0,0 +1,34 @@
+#include iostream
+using namespace std;
+
+class IOSys{
+public
+    double process();
+    char input;
+    char output;
+
+    IOSys(); #1
+    IOSys(double x); #2
+
+};
+
+IOSysIOSys(){ #1
+    coutInputNonenOutputNoneendl;
+}
+
+IOSysIOSys(double x){ #2
+    input=x;
+    output=process();
+    coutInputOutputendl;
+}
+
+double IOSysprocess(){
+    return ((inputinput)2);
+}
+
+int main(){
+    IOSys ino; #1
+
+    IOSys inoo(6.0);#2
+
+}
